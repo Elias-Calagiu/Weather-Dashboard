72285f00f800e4d911efef99dd50c7db7a15011a
Elias Calagiu <eliasc81@gmail.com>
2020-12-20 19:44:26 -0800

    added API key, created an AJAX call and a variable to hold city search information

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ var APIKey = "8f79ebd7aceb569775a85f8feb8ed132";
 
 // Here I create a variable to hold which city the user searches for
 
-// var city= $("#searchBtn").val();
+var city= $("#searchBtn").val();
 
 // Here we are building the URL we need to query the database
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + APIKey;
 
 // We then created an AJAX call
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// }).then(function(response) {
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
 
 //   // Create CODE HERE to Log the queryURL
 //   // Create CODE HERE to log the resulting object
@@ -22,4 +22,4 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + API
 //   // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
 //   // Create CODE HERE to dump the temperature content into HTML
 
-// });
+});
