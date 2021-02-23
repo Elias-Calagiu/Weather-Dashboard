4a971b79764d14f9c3c948fcddc7a3765bb657de
Elias Calagiu <eliasc81@gmail.com>
2021-02-23 07:01:57 -0800

    5-day forecast appends next day to card

--- script.js
+++ script.js
@@ -81,37 +81,28 @@ $("#forecastSearchBtn").click(function () {
         // add temp content to html
         // $("#tempF").text("Temperature (F): " + tempF.toFixed(2));
 
-        var forecast = "";
+        // var forecast = "";
+        // $("#city").html("<h3>" + response.name + " Weather Details</h3>");
+        // var forecastDate = response.list[i].dt_txt
+        // var forecastWind= $("#forecastWind").text("Wind Speed: " + response.list[i].wind.speed);
+        // var forecastHumidity= $("#forecastHumidity").text("Humidity: " + response.list[i].main.humidity);
+        // var forecastDescription= $("#forecastDescription").text("Description: " + response.list[i].weather[0].description);
+        // var forecastTempF = (response.list[i].main.temp - 273.15) * 1.80 + 32;
+
+        // add temp content to html
+        // $("#tempF").text("Temperature (F): " + tempF.toFixed(2));
         for (let i = 0; i < response.list.length; i = i + 8) {
-            forecast += `
-            <div class="card" style="width: 18rem;">
-        <p>${response.list[i].dt_txt}</p>
-            <h6>Description :  ${response.list[i].weather[0].description}</h6>
-            <h6>Temp: ${response.list[i].main.temp} <img src="https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png" /></h6>
-            <h6>Humidity : ${response.list[i].main.humidity}</h6>
-            <p> Wind Speed: ${response.list[i].wind.speed}</p>
-            </div>`
+            // forecast += `
+            response.list[i].dt_txt
+            $("#forecastWind").text("Wind Speed: " + response.list[i].wind.speed);
+            $("#forecastHumidity").text("Humidity: " + response.list[i].main.humidity);
+            $("#forecastDescription").text("Description: " + response.list[i].weather[0].description);
+            var forecastTemp = (response.list[i].main.temp - 273.15) * 1.80 + 32;
+            $("#forecastTemp").text("Temperature (F): " + forecastTemp.toFixed(2));
         }
-        $("#forecast").html(forecast)
+        // $("#forecast").html(forecast)
     });
 
-    //     $("#tempF2").text("Temperature (F): " + tempF.toFixed(2));
+    
 });
-
-// We then created an AJAX call
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// }).then(function(response) {
-//     // preventDefault();
-// // console.log(queryURL);
-// console.log(response)
-//   // Create CODE HERE to Log the queryURL
-//   // Create CODE HERE to log the resulting object
-//   // Create CODE HERE to calculate the temperature (converted from Kelvin)
-//   // Create CODE HERE to transfer content to HTML
-//   // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-//   // Create CODE HERE to dump the temperature content into HTML
-
-
 
