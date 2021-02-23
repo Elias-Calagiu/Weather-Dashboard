a3ca32b0b8bfbe71c7e9e696b0c63185d61bd750
Elias Calagiu <eliasc81@gmail.com>
2021-02-23 06:47:14 -0800

    added moment function and new api call for forecast

--- script.js
+++ script.js
@@ -4,49 +4,100 @@ var apiKey = "&appid=24875e6cac4fb38c4b7d718765abc64f";
 
 // Here I create a variable to hold which city the user searches for
 
-var cityArray=[]
+var cityArray = []
 
 var localStorage = localStorage.getItem("cities")
-// Create a function to show info when search is entered
 
-$("#searchBtn").click(function(){
+var moment = moment().format("dddd, MMMM Do YYYY")
+console.log(moment);
+$(".date").text(moment);
+
+// Create a function to show info when search is entered for current day
+
+$("#searchBtn").click(function () {
     // preventDefault();
-    city= $("#search")
-    .val()
-    .trim();
+    city = $("#search")
+        .val()
+        .trim();
     console.log(city)
 
-    if (city){
+    if (city) {
         cityArray.push(city);
         localStorage.setItem("cities", JSON.stringify(cityArray))
+        // for (let i = 0; i < cityArray.length; i++) {
+        //     $("#city2").append(cityArray[i])            
+        // }
+
     }
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey
     $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response) {
-          // preventDefault();
-      // console.log(queryURL);
-      console.log(response)
-      $("#city").html("<h3>" + response.name + " Weather Details</h3>");
-      $("#wind").text("Wind Speed: " + response.wind.speed);
-      $("#humidity").text("Humidity: " + response.main.humidity);
-      $("#description").text("Description: " + response.weather[0].description);
-      var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    }).then(function (response) {
+        // preventDefault();
+        // console.log(queryURL);
+        console.log(response)
+        $("#city").html("<h3>" + response.name + " Weather Details</h3>");
+        $("#wind").text("Wind Speed: " + response.wind.speed);
+        $("#humidity").text("Humidity: " + response.main.humidity);
+        $("#description").text("Description: " + response.weather[0].description);
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
 
         // add temp content to html
-        $("#tempF").text("Temperature (F) " + tempF.toFixed(2));
-      $("#city2").html("<h3>" + response.name + " Weather Details</h3>");
-      $("#wind2").text("Wind Speed: " + response.wind.speed);
-      $("#humidity2").text("Humidity: " + response.main.humidity);
-      $("#description2").text("Description: " + response.weather[0].description);
-      var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $("#tempF").text("Temperature (F): " + tempF.toFixed(2));
+
+    });
+
+});
+$("#forecastSearchBtn").click(function () {
+    // preventDefault();
+    city = $("#search")
+        .val()
+        .trim();
+    console.log(city)
+
+    // if (city) {
+    //     cityArray.push(city);
+    //     localStorage.setItem("cities", JSON.stringify(cityArray))
+    //     // for (let i = 0; i < cityArray.length; i++) {
+    //     //     $("#city2").append(cityArray[i])            
+    //     // }
+
+    // }
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // preventDefault();
+        // console.log(queryURL);
+        console.log(response)
+        // $("#city").html("<h3>" + response.name + " Weather Details</h3>");
+        // $("#wind").text("Wind Speed: " + response.wind.speed);
+        // $("#humidity").text("Humidity: " + response.main.humidity);
+        // $("#description").text("Description: " + response.weather[0].description);
+        // var tempF = (response.main.temp - 273.15) * 1.80 + 32;
 
         // add temp content to html
-        $("#tempF2").text("Temperature (F) " + tempF.toFixed(2));
+        // $("#tempF").text("Temperature (F): " + tempF.toFixed(2));
 
+        var forecast = "";
+        for (let i = 0; i < response.list.length; i = i + 8) {
+            forecast += `
+            <div class="card" style="width: 18rem;">
+        <p>${response.list[i].dt_txt}</p>
+            <h6>Description :  ${response.list[i].weather[0].description}</h6>
+            <h6>Temp: ${response.list[i].main.temp} <img src="https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png" /></h6>
+            <h6>Humidity : ${response.list[i].main.humidity}</h6>
+            <p> Wind Speed: ${response.list[i].wind.speed}</p>
+            </div>`
+        }
+        $("#forecast").html(forecast)
+    });
+
+    //     $("#tempF2").text("Temperature (F): " + tempF.toFixed(2));
 });
-});
+
 // We then created an AJAX call
 // $.ajax({
 //   url: queryURL,
